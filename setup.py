487597b56d2c56d309cbf159b0f5a29df7792fd4
krrr <guogaishiwo@gmail.com>
2014-05-29 22:40:17 +0800

    Change project structure

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ program_path = path.dirname(path.realpath(__file__))
 import PySide
 pyside_dir = path.dirname(PySide.__file__)
 # prepare translation files
-trs = [i for i in glob('lang/*.qm')]
-qt_trs = [path.join(pyside_dir, 'translations', 'qt_%s')
-          % path.basename(i) for i in trs]
-all_trans = [(i, '../lang/%s' % path.basename(i)) for i in trs + qt_trs]
+ts = [i for i in glob('hazama/lang/*.qm')]
+qt_ts = [path.join(pyside_dir, 'translations', 'qt_%s')
+         % path.basename(i) for i in ts]
+all_ts = [(i, '../lang/%s' % path.basename(i)) for i in ts + qt_ts]
 
 
-main = Executable('hazama.py',
+main = Executable('hazama/hazama.py',
                   base='Win32GUI',
                   icon='res/appicon/appicon.ico',
                   appendScriptToLibrary=False,
@@ -24,10 +24,11 @@ main = Executable('hazama.py',
                   
 freeze_opts = dict(
     options={'build_exe': {
-        'include_files': all_trans,
+        'include_files': all_ts,
         'includes': ['PySide.QtCore', 'PySide.QtGui'],
         'excludes': ['tkinter', 'PySide.QtNetwork', 'distutils'],
         'build_exe': 'build/lib',
+        'path': sys.path + ['hazama/'],
         'init_script': path.join(program_path, 'utils', 'cx_freeze_init.py')}},
     executables=[main])
     
