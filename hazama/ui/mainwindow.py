34792e9106bfdf8debc1a7f2eaa34c91ee91b5a5
krrr <guogaishiwo@gmail.com>
2015-03-21 15:43:17 +0800

    Fix Heat Map stretch factor bug

--- hazama/ui/mainwindow.py
+++ hazama/ui/mainwindow.py
@@ -117,8 +117,8 @@ class MainWindow(QMainWindow, Ui_mainWindow):
         ratio = {QLocale.Chinese: 1, QLocale.English: 5, QLocale.Japanese: 1.2,
                  }.get(QLocale().language(), 1)
         logging.debug('HeatMap got length ratio %s' % ratio)
-        descriptions = ('0', '< %d' % 200 * ratio, '< %d' % 550 * ratio,
-                        '>= %d' % 550 * ratio)
+        descriptions = ('0', '< %d' % (200 * ratio), '< %d' % (550 * ratio),
+                        '>= %d' % (550 * ratio))
 
         def colorFunc(y, m, d, cellColors):
             data = colorFunc.cached.get((y, m, d), 0)
