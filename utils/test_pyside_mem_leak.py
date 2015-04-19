39403eecea9fbab887a796b97ad6b43ad20d35fd
krrr <guogaishiwo@gmail.com>
2015-04-19 10:28:20 +0800

    Add misc file

---
+++ utils/test_pyside_mem_leak.py
@@ -0,0 +1,33 @@
+from PySide.QtCore import *
+from PySide.QtGui import *
+
+"""Do not pass parent to some object that isn't sub-class of QWidget
+when not necessary.
+
+Detail: Some widget never release its 'functional' child, such as
+ItemDelegate(with parent set to the view) set by QListView.setItemDelegate
+and replaced by other delegate later.
+Call deleteLater manually will solve this partially."""
+
+
+class DummyDelegate(QAbstractItemDelegate):
+    def __init__(self, p=None):
+        super(DummyDelegate, self).__init__(p)
+        self.w = QWidget()
+
+    def paint(self, *args, **kwargs):
+        pass
+
+    def sizeHint(self, *args, **kwargs):
+        return QSize()
+
+app = QApplication(['a'])
+list = QListView()
+for _ in range(10000):
+    d = DummyDelegate(list)  # here, with list it takes 26MB, and 13MB if pass None
+    list.setItemDelegate(d)
+    list.setItemDelegate(None)
+    del d
+
+
+app.exec_()
