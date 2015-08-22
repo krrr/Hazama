832ac96b4a3125d0be46e93e451b7138ad1a2ec2
krrr <guogaishiwo@gmail.com>
2015-08-22 19:34:54 +0800

    Merge main entry into __init__.py

--- hazama/__init__.py
+++ hazama/__init__.py
@@ -9,6 +9,32 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
-__version__ = '0.31'
+__version__ = '0.31.3'
 __desc__ = 'A simple cross-platform diary application'
 __author__ = 'krrr'
+
+
+def main_entry():
+    import time
+    import logging
+    from hazama import db, config
+
+    start_time = time.clock()
+    config.changeCWD()
+    config.init()
+    # setup logging
+    logging.basicConfig(
+        format='%(levelname)s: %(message)s', level=logging.DEBUG if
+        config.settings['Main'].getboolean('debug') else logging.INFO)
+    logging.info('Hazama v%s', __version__)
+    logging.info(str(config.nikki))
+
+    from hazama.ui import app  # initialize UI
+    from hazama.ui.mainwindow import MainWindow
+    w = MainWindow()
+    w.show()
+    logging.debug('startup took %.2f sec', time.clock()-start_time)
+
+    if config.settings['Main'].getboolean('backup', True):
+        db.check_backup()
+    return app.exec_()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # this file is used to do testing without installing or build exe
 import sys
-from hazama.mainentry import main
+from hazama import main_entry
 
-sys.exit(main())
+sys.exit(main_entry())
