78df5fc3ec10c00d970d123a9df463e5623d0add
krrr <guogaishiwo@gmail.com>
2015-09-20 01:06:17 +0000

    Use shorter description on windows exe file

--- utils/setupfreeze.py
+++ utils/setupfreeze.py
@@ -1,4 +1,4 @@
-"""Only used to generate freeze binary because of limitation of cx_Freeze."""
+"""Only used to generate frozen binary because of limitation of cx_Freeze."""
 import sys
 import os
 from os import path
@@ -6,14 +6,13 @@ from glob import glob
 import PySide
 from cx_Freeze import setup, Executable
 
-sys.path[0] = os.getcwd()  # this script will called by ../setup.py
-from hazama import __version__, __author__, __desc__
-
+sys.path[0] = os.getcwd()  # this script will be called by ../setup.py
+from hazama import __version__, __author__
 pyside_dir = path.dirname(PySide.__file__)
 
 
 # prepare translation files
-ts = list(glob('hazama/lang/*.qm'))  # application's translations
+ts = list(glob('hazama/lang/*.qm'))  # app's translations
 ts += [path.join(pyside_dir, 'translations', 'qt_%s')
        % path.basename(i) for i in ts]  # corresponding Qt translations
 all_ts = [(i, '../lang/%s' % path.basename(i)) for i in ts]
@@ -25,7 +24,10 @@ main = Executable('hazama.py',
                   targetDir='build')
 
 setup(
-    name='Hazama', author=__author__, version=__version__, description=__desc__,
+    name='Hazama',
+    author=__author__,
+    version=__version__,
+    description='Hazama',
     options={'build_exe': {
         'include_files': all_ts,
         'includes': ['PySide.QtCore', 'PySide.QtGui', 'hazama'],
